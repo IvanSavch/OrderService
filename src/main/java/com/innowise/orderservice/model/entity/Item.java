8494414114ef4115a84e8614242b6281d215a604
Savva <sav.van.ivan3@mail.ru>
2026-02-09 11:23:10 +0300

    Created DataBase and entity

--- src/main/java/com/innowise/orderservice/model/entity/Item.java
+++ src/main/java/com/innowise/orderservice/model/entity/Item.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import lombok.Setter;
 import java.math.BigDecimal;
 
 @Entity
+@Table(name = "items")
 @Setter
 @Getter
 @AllArgsConstructor
--- src/main/java/com/innowise/orderservice/model/entity/Order.java
+++ src/main/java/com/innowise/orderservice/model/entity/Order.java
@@ -1,15 +1,21 @@
 package com.innowise.orderservice.model.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Entity
+@Table(name = "orders")
 @Setter
 @Getter
 @AllArgsConstructor
@@ -22,5 +28,7 @@ public class Order extends Auditable {
     private String status;
     private Long totalPrice;
     private Boolean deleted;
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> list;
 
 }
