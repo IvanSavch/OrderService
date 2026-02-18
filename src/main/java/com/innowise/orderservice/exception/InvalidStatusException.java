dced7b555aab12592d07c992145ffd4b7e64da0a
Savva <sav.van.ivan3@mail.ru>
2026-02-18 18:13:08 +0300

    Fixed

--- src/main/java/com/innowise/orderservice/exception/InvalidStatusException.java
+++ src/main/java/com/innowise/orderservice/exception/InvalidStatusException.java
@@ -1,7 +1,7 @@
 package com.innowise.orderservice.exception;
 
 public class InvalidStatusException extends RuntimeException {
-    private static final String DEFAULT_MESSAGE = "Status must ber CREATED,IN_PROGRESS OR DELIVERED";
+    private static final String DEFAULT_MESSAGE = "Status must ber CREATED,IN_PROGRESS,DELIVERED,CANCELLED,PAID";
     public InvalidStatusException() {
         super(DEFAULT_MESSAGE);
     }
