d17af0e3a00f17772ed57db94aa97e06d6cf1dfa
Savva <sav.van.ivan3@mail.ru>
2026-02-23 22:52:31 +0300

    Added indexes Added auditing column Implemented soft delete Fixed OrderUpdateDto Added an exception handler for ServiceUnavailableException Added unit tests for ItemService and UserClient

--- src/test/java/com/innowise/orderservice/service/impl/OrderServiceImplTest.java
+++ src/test/java/com/innowise/orderservice/service/impl/OrderServiceImplTest.java
@@ -2,6 +2,7 @@ package com.innowise.orderservice.service.impl;
 
 import com.innowise.orderservice.client.UserClient;
 import com.innowise.orderservice.exception.OrderNotFoundException;
+import com.innowise.orderservice.mapper.OrderItemMapper;
 import com.innowise.orderservice.mapper.OrderMapper;
 import com.innowise.orderservice.model.dto.UserDto;
 import com.innowise.orderservice.model.dto.order.OrderCreateDto;
@@ -10,6 +11,7 @@ import com.innowise.orderservice.model.dto.order.OrderUpdateDto;
 import com.innowise.orderservice.model.dto.orderitem.OrderItemDto;
 import com.innowise.orderservice.model.entity.Item;
 import com.innowise.orderservice.model.entity.Order;
+import com.innowise.orderservice.model.entity.OrderItem;
 import com.innowise.orderservice.repository.OrderRepository;
 import com.innowise.orderservice.service.ItemService;
 import org.junit.jupiter.api.Test;
@@ -25,6 +27,7 @@ import org.springframework.data.jpa.domain.Specification;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
@@ -41,6 +44,8 @@ class OrderServiceImplTest {
     private OrderRepository orderRepository;
     @Mock
     private OrderMapper orderMapper;
+    @Mock
+    private OrderItemMapper orderItemMapper;
     @Mock
     private UserClient userClient;
     @Mock
@@ -70,10 +75,18 @@ class OrderServiceImplTest {
         Order savedOrder = new Order();
         savedOrder.setId(1L);
 
+        OrderItem orderItem = new OrderItem();
+        orderItem.setId(1L);
+
+        savedOrder.setList(List.of(orderItem));
+
         when(userClient.findByEmail("test@mail.com")).thenReturn(userDto);
         when(itemService.findById(1L)).thenReturn(item);
         when(orderRepository.save(any(Order.class))).thenReturn(savedOrder);
+        when(orderMapper.toOrder(any(OrderCreateDto.class))).thenReturn(savedOrder);
+        when(orderItemMapper.toOrderItem(any(OrderItemDto.class))).thenReturn(orderItem);
         when(orderMapper.toResponse(any(Order.class), eq(userDto))).thenReturn(new OrderResponseDto());
+
 
         OrderResponseDto result = orderService.create(createDto);
 
@@ -98,6 +111,7 @@ class OrderServiceImplTest {
         OrderResponseDto result = orderService.findById(1L);
         assertNotNull(result);
     }
+
     @Test
     void findByIdOrderNotFound() {
         when(orderRepository.findById(1L)).thenReturn(Optional.empty());
@@ -117,7 +131,7 @@ class OrderServiceImplTest {
         Order order2 = new Order();
         order2.setId(2L);
         order2.setUserId(userDto.getId());
-        List<Order> orderList = List.of(order1,order2);
+        List<Order> orderList = List.of(order1, order2);
 
         when(userClient.findById(1L)).thenReturn(userDto);
         when(orderRepository.findByUserId(1L)).thenReturn(orderList);
@@ -130,6 +144,7 @@ class OrderServiceImplTest {
         verify(orderRepository).findByUserId(1L);
         verify(userClient).findById(1L);
     }
+
     @Test
     void findByUserIdThrowOrderNotFound() {
         Long userId = 2L;
@@ -154,7 +169,7 @@ class OrderServiceImplTest {
         order2.setUserId(userDto.getId());
         order2.setStatus(Order.OrderStatus.CREATED);
 
-        List<Order> orders = List.of(order,order2);
+        List<Order> orders = List.of(order, order2);
         Page<Order> page = new PageImpl<>(orders);
 
         when(orderRepository.findAll(any(Specification.class), eq(pageable))).thenReturn(page);
@@ -181,6 +196,10 @@ class OrderServiceImplTest {
         order.setUserId(userDto.getId());
         order.setList(new java.util.ArrayList<>());
 
+        OrderItem orderItem = new OrderItem();
+        orderItem.setId(1L);
+        orderItem.setOrder(order);
+        orderItem.setItem(item);
         OrderUpdateDto updateDto = new OrderUpdateDto();
         updateDto.setStatus(Order.OrderStatus.DELIVERED);
 
@@ -193,6 +212,7 @@ class OrderServiceImplTest {
         when(orderRepository.findById(1L)).thenReturn(Optional.of(order));
         when(itemService.findById(1L)).thenReturn(item);
         when(userClient.findById(1L)).thenReturn(userDto);
+        when(orderItemMapper.toOrderItem(any(OrderItemDto.class))).thenReturn(orderItem);
         when(orderMapper.toResponse(order, userDto)).thenReturn(new OrderResponseDto());
 
         OrderResponseDto orderResponseDto = orderService.updateById(1L, updateDto);
@@ -211,8 +231,10 @@ class OrderServiceImplTest {
 
         orderService.deleteById(1L);
 
-        verify(orderRepository).delete(order);
+        assertTrue(order.getDeleted());
+        verify(orderRepository).save(order);
     }
+
     @Test
     void deleteByIdOrderNotFound() {
         when(orderRepository.findById(1L)).thenReturn(Optional.empty());
