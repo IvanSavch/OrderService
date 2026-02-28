879a6b1f9babccced4c3ad1f7a1eb3dcd452b520
Savva <sav.van.ivan3@mail.ru>
2026-02-28 22:47:00 +0300

    Change compose Changed AuthenticationServiceImpl Changed UserClient Changed jwt filter

--- src/main/java/com/innowise/orderservice/client/UserClient.java
+++ src/main/java/com/innowise/orderservice/client/UserClient.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
